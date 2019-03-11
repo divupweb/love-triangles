f8ebdfa57c0a9ff868c183c794b2a0e91e0c57e9
divupweb <divupweb@gmail.com>
2019-03-11 21:24:51 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var trianglesCount = 0;
+  for(var i=0;i<preferences.length;i++){
+    var firstTriangle = preferences[i];
+    var secondTriangle = preferences[firstTriangle-1];
+    var thirdTriangle = preferences[secondTriangle-1];
+    if(thirdTriangle-1 == i && i+1!=firstTriangle ){
+      trianglesCount++;
+    }
+  }
+  return trianglesCount/3;
 };
